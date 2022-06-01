9631c93e027d43a7ce4f56c5afd266f9e488ea61
Danmuse <danifabriziodmodica@gmail.com>
2022-06-01 01:41:05 -0300

    Shuffling Cards

--- Exercises/Shuffling Cards/Shuffling.c
+++ Exercises/Shuffling Cards/Shuffling.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdint.h>
+// #include <stdint.h>
 // El único contenido importante de '<stdint.h>' es...
 
 #ifdef _UINT8_TYPE_
@@ -11,8 +11,8 @@ typedef _UINT8_TYPE_ uint8_t;
 struct card {
 	// 'number' y 'naipe' van a ser punteros porque tendrán que ser
 	// referenciados por un vector.
-	const uint8_t *number;
-	const uint8_t *naipe;
+	const char *number;
+	const char *naipe;
 };
 
 // Defino a "Card" como la estructura de una carta.
@@ -24,7 +24,7 @@ void handOut(const Card *const);
 
 int main(void) {
 	Card deck[48];
-
+	
 	// Un vector es un array unidimencional, que utiliza un índice para
 	// referenciar a cada uno de los elementos.
 	// Cuando ya son inicializados, no es necesario indicarles el tamaño.
@@ -48,9 +48,21 @@ void assembleDeck(Card *const ptrDeck, const char *ptrNumber[], const char *ptrN
 }
 
 void shuffle(Card *const ptrDeck) {
+	int cant, i, j;
+	Card aux_Card;
 	
+	for (i = 0; i < 48; i++) {
+		j = rand() % 48;
+		aux_Card = ptrDeck[i];
+		ptrDeck[i] = ptrDeck[j];
+		ptrDeck[j] = aux_Card;
+	}
 }
 
 void handOut(const Card *const ptrDeck) {
-	
+	int i, j;
+	printf("\nPrimer Jugador:\n");
+	for (i = 0; i < 3; i++) printf("%s de %s\n", ptrDeck[i].number, ptrDeck[i].naipe);
+	printf("\nSegundo Jugador:\n");
+	for (j = 3; j < 6; j++) printf("%s de %s\n", ptrDeck[j].number, ptrDeck[j].naipe);
 }
