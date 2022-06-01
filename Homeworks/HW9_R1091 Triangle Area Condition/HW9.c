dea1c921f695d1a8a89d8b135643138ec7cf496b
Danmuse <danifabriziodmodica@gmail.com>
2022-06-01 01:39:50 -0300

    Triangle Area Condition

--- Homeworks/HW9_R1091 Triangle Area Condition/HW9.c
+++ Homeworks/HW9_R1091 Triangle Area Condition/HW9.c
@@ -9,40 +9,48 @@
 #include <math.h>
 #define TRUE 1
 
-int area(int, int, int, float *);
+int calc_area(float, float, float, float *);
 
 int main(void) {
-	int INPUT, output, side_1, side_2, side_3;
-	float r_area = 0, max_area = 0;
+	int INPUT, output, i, pos = 0;
+	float r_area = 0, max_area = 0, side_1, side_2, side_3;
+	float *sides[] = { &side_1, &side_2, &side_3 };
 	printf("\nIngrese 3 valores que correspondan a los lados de un triangulo...\n");
-	while(TRUE) {
-		printf("Valor #1: ");
-		output = scanf("%d", &side_1);
-		printf("Valor #2: ");
-		output = scanf("%d", &side_2);
-		printf("Valor #3: ");
-		output = scanf("%d", &side_3);
-		output == 0 ? printf("\n\nAlguno de los valores ingresados no son correctos\n") :
-		area(side_1, side_2, side_3, &r_area);
-		INPUT = area(side_1, side_2, side_3, &r_area);
-		if(INPUT == 0) {
-			if (r_area > max_area) max_area = r_area;
-		} else break;
-		printf("Area: %.2f\n", r_area);
+	while (TRUE) {
+		for (i = 0; i < 3; i++) {
+			printf("Valor #%i: ", i);
+			output = scanf("%f", sides[pos]);
+			pos++;
+		}
+		
+		if (output == 0) {
+			printf("\nAlguno de los valores ingresados no son correctos\n");
+			break;
+		}	else calc_area (side_1, side_2, side_3, &r_area);
+		
+		INPUT = calc_area(side_1, side_2, side_3, &r_area);
+		
+		if (r_area > max_area) max_area = r_area; 
+		if (INPUT == 1) break;
+		printf("Area: %.2f\n\n", r_area);
+		pos = 0;
 	}
-	printf("Los datos ingresados no validan que sea un triangulo\n\n");
-	printf("El area maxima de las ternas ingresadas fue %.2f\n", max_area);
+	
+	printf("\nLos datos ingresados no validan que sea un triangulo\n");
+	printf("\nEl area maxima de las ternas ingresadas fue %.2f\n", max_area);
 	return 0;
 }
 
-int area(int a, int b, int c, float *area) {
-	int mp_a, mp_b, mp_c, debug = 0;
+int calc_area(float a, float b, float c, float *area) {
+	int debug = 0;
+	float mp_a, mp_b, mp_c, radicando;
 	mp_a = ((a + b + c) / 2) - a;
 	mp_b = ((a + b + c) / 2) - b;
 	mp_c = ((a + b + c) / 2) - c;
-	if (a + b > c) *area = sqrt(((a + b + c) / 2) * mp_a * mp_b * mp_c);
-	if (a + c > b) *area = sqrt(((a + b + c) / 2) * mp_a * mp_b * mp_c);
-	if (b + c > a) *area = sqrt(((a + b + c) / 2) * mp_a * mp_b * mp_c);
-	else debug = 1;
+	radicando = (((a + b + c) / 2) * mp_a * mp_b * mp_c);
+
+	*area = sqrt(radicando);
+
+	if (radicando <= 0) debug = 1;	// Area = Unidad Imaginaria o Inexistente
 	return debug;
 }
