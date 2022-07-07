2e9130cbcbc469669457498809ffa04f32da0041
Danmuse <danifabriziodmodica@gmail.com>
2022-07-07 17:25:43 -0300

    STRING PRACTICE

---
+++ Homeworks/HW18_R1091 STRING PRACTICE/HW18.c
@@ -0,0 +1,18 @@
+/**********************************************************************************
+ * Inicializar una cadena con '\0'                                                *
+ * Contar la ocurrencia de un caracter en una cadena                              *
+ * Invertir la cadena                                                             *
+ * Convertir una cadena a mayusculas                                              *
+ * Convertir una cadena a minusculas                                              *
+ * Convertir una cadena a Titulo                                                  *
+ * Remover todos los espacios                                                     *
+ * Contar la cantidad de palabras                                                 *
+ * Verificar si una cadena tiene todos sus caracteres alfanumericos (a-z A-Z 0-9) *
+ * Verificar si en cadena todos los caracteres son numeros (0-9)                  *
+ **********************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+	return 0;
+}
