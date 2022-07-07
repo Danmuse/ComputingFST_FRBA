9e9b21191f9398971b79bc49a51461f803a82176
Danmuse <danifabriziodmodica@gmail.com>
2022-07-07 17:25:20 -0300

    ARRAY PRACTICE

---
+++ Homeworks/HW17_R1091 ARRAY PRACTICE/HW17.c
@@ -0,0 +1,24 @@
+/****************************************************************************************************
+ * init_arr(int a[], int max_size) Inicializar todos los valores en '0'                             *
+ * load_arr(int a[], int max_size) Ingreso por teclado hasta el max_size o hasta que se ingrese '0' *
+ * size_arr(int a[], int max_size) Devuelve el tamaño del array                                     *
+ * has_value(int a, int b[]) Buscar el valor de 'a' en 'b'                                          *
+ * get_value(int a, int b[], int max_size) Inserta un elemento al final del array                   *
+ * print_arr(int a[]) Muestra el array                                                              *
+ * union_arrs(int a[], int b[], int c[], int max_size) Copia el array 'b' y 'c' en 'a'              *
+ ****************************************************************************************************
+ * NOTA: Se considera que toda posicion del arreglo que tiene un '0' no es un elemento del arr.     *
+ * Un arreglo con TAMANO lleno con valores '0' se lo considera arr vacio.                           *
+ ****************************************************************************************************/
+
+#include <stdio.h>
+#include "myString.h"
+#define SIZE 10
+
+int main(void) {
+	int arr[SIZE];
+	init_arr(arr, SIZE);
+	load_arr(arr, SIZE);
+	print_arr(arr);
+	return 0;
+}
---
+++ Homeworks/HW17_R1091 ARRAY PRACTICE/myString.h
@@ -0,0 +1,20 @@
+/****************************************************************************************************
+ * init_arr(int a[], int max_size) Inicializar todos los valores en '0'                             *
+ * load_arr(int a[], int max_size) Ingreso por teclado hasta el max_size o hasta que se ingrese '0' *
+ * size_arr(int a[], int max_size) Devuelve el tamaño del array                                     *
+ * has_value(int a, int b[]) Buscar el valor de 'a' en 'b'                                          *
+ * get_value(int a, int b[], int max_size) Inserta un elemento al final del array                   *
+ * print_arr(int a[]) Muestra el array                                                              *
+ * union_arrs(int a[], int b[], int c[], int max_size) Copia el array 'b' y 'c' en 'a'              *
+ ****************************************************************************************************
+ * NOTA: Se considera que toda posicion del arreglo que tiene un '0' no es un elemento del arr.     *
+ * Un arreglo con TAMANO lleno con valores '0' se lo considera arr vacio.                           *
+ ****************************************************************************************************/
+ 
+void init_arr(int a[], int max_size);
+void load_arr(int a[], int max_size);
+int size_arr(int a[], int max_size);
+int has_value(int a, int b[]);
+int get_value(int a, int b[], int max_size);
+void print_arr(int a[]);
+int union_arrs(int a[], int b[], int c[], int max_size);
