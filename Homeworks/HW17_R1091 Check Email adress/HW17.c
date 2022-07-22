c4c913e8adc66b96d9dcda2b4fbb0267502fd6d3
Danmuse <danifabriziodmodica@gmail.com>
2022-07-21 22:15:23 -0300

    Check Email adress

---
+++ Homeworks/HW17_R1091 Check Email adress/HW17.c
@@ -0,0 +1,11 @@
+/****************************************************************************************
+ * Realizar una funciona para validar que una cadena es un email int es_email(char *s); *
+ * Devuelve (int) 1 si el string especificado es un email.                              *
+ * Debe cumplir que contenga el caracter '@' y al menos un carater '.' luego.           *
+ ****************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+	return 0;
+}
