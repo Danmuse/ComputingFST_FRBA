d366c8b6256237a2124e7541f6133830df0cf21b
Danmuse <danifabriziodmodica@gmail.com>
2022-07-21 22:14:53 -0300

    Email adress

---
+++ Homeworks/HW16_R1091 Email adress/HW16.c
@@ -0,0 +1,11 @@
+/*****************************************************************************************
+ * Solicitar por ingreso de teclado el nombre, apellido y general una direccion de email *
+ * cumpliendo la siguiente regla: Primer letra del nombre, 8 primeras letra del apellido *
+ * y finalizando con "@frba.utn.edu.ar"                                                  *
+ *****************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+	return 0;
+}
