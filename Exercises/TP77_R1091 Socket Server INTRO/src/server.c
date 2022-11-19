5eecb6edad9a4b765ad140cbcaed4997a77a4d6b
Danmuse <danifabriziodmodica@gmail.com>
2022-11-19 19:52:39 -0300

    Socket Server

--- Exercises/TP77_R1091 Socket Server INTRO/src/server.c
+++ Exercises/TP77_R1091 Socket Server INTRO/src/server.c
@@ -1,26 +1,22 @@
 #include "sock-lib.h"
 
-int main (void)
-{
-	int sockfd; /* File Descriptor del socket por el que el servidor "escuchará" conexiones*/
+int main(void) {
+	int sockfd; // File Descriptor del socket por el que el servidor "escuchará" conexiones
 	char message[] = "Hello, world!";
-	struct sockaddr_in my_addr;	/* contendrá la dirección IP y el número de puerto local */
+	struct sockaddr_in my_addr; // Contendrá la dirección IP y el número de puerto local
 	int sockdup; 
 
-	if ((sockfd = Open_conection (&my_addr)) == -1)
-	{
+	if ((sockfd = openConnection(&my_addr)) == -1) {
 		perror ("Falló la creación de la conexión"); 
 		exit (1);
 	}
 
-	while(1)
-	{
-		sockdup = Aceptar_pedidos (sockfd);
-// Aca se pone un fork () y se lo convierte en un server concurrente
+	while(1) {
+		sockdup = acceptRequest(sockfd);
+// Aca se pone un fork() y se lo convierte en un server concurrente
 // En tal caso, lo que sigue es el child process
 // El padre vuelve a Aceptar_pedidos
-		if (write (sockdup, message , sizeof (message)) == -1)
-		{
+		if (write(sockdup, message , sizeof(message)) == -1) {
 			perror("Error escribiendo mensaje en socket");
 			exit (1);
 		}
