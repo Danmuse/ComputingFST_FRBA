8cd2eea4a22d30c49774f6400560d479a7611b2d
Danmuse <danifabriziodmodica@gmail.com>
2022-11-20 01:10:49 -0300

    QSORT

---
+++ Exercises/TP58_R1091 QSORT/compiler.sh
@@ -0,0 +1,3 @@
+rm -f test.out
+gcc -Wall -pedantic main.c -o test.out
+time ./test.out
--- Exercises/TP58_R1091 QSORT/main.c
+++ Exercises/TP58_R1091 QSORT/main.c
@@ -1,3 +1,5 @@
+/*
+
 #include <stdio.h>
 #include <stdlib.h>
 #define ARRAY_SIZE 7
@@ -10,74 +12,21 @@ int comp(int*, int*);
 void printArray(uint8_t[], size_t);
 
 int main(void) {
-	size_t i;
+	size_t index, j_index;
 	uint8_t data[ARRAY_SIZE] = {7, 1, 3, 5, 2, 8, 10}
 	qsort(data, sizeof(int), sizeof(data) / sizeof(uint8_t), comp);
-	for (i = 0; i < sizeof(data)/sizeof(int); i++) printf("data[%ld] = %d\n", i, data[i]);
+	// for (index = 0; index < ARRAY_SIZE - 1; index++) {
+	//     for (j_index = index + 1; j_index < ARRAY_SIZE; j_index++)
+	//          if (array[j_index] < array[j_index]) swap(&array[j_index], &array[j_index]);
+	// }
+	for (index = 0; index < sizeof(data)/sizeof(int); index++) printf("data[%ld] = %d\n", index, data[index]);
 	return EXIT_SUCCESS;
-}
-
-int 
-
-for (i = 0; i < (size -1); i++) {
-	for(j = i + 1; j < size; j++) {
-		// if (vec[i] < vec[j])  { 
-			swap(&vec[i], &vec[j]);
-			hubo_cambio = true;
-		}
-	}
-	hubo_cambio = false;
-}
-
-void swap(int*, int*) {
-	
-}
-
-int comp(int* a, int* b) {
-	return *a > *b;
-}
-
-/*
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-void swap(int *, int *);
-
-void printArray(int *, int);
-
-int compare(int *, int *);
-
-#define ARRAY_SIZE 100000
-
-int main() {
-    int array[ARRAY_SIZE];
-    int i;
-    int j;
-
-    srand(getpid());
-
-    for (i = 0; i < ARRAY_SIZE; i++) array[i] = rand() % 20;
-    qsort(array, ARRAY_SIZE, 4, compare);
-//    for (i = 0; i < ARRAY_SIZE - 1; i++) {
-//        for (int j = i + 1; j < ARRAY_SIZE; j++) {
-//            if (array[i] < array[j]) swap(&array[i], &array[j]);
-//        }
-//    }
-    printf("array:\n");
-    printArray(array, ARRAY_SIZE);
-    return 0;
 }
 
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
-}
-
-void printArray(int *arr, int size) {
-    for (int i = 0; i < size; ++i) printf("Elem %d: %d\n", i, arr[i]);
 }
 
 int compare(int *a, int *b) {
@@ -87,3 +36,47 @@ int compare(int *a, int *b) {
 }
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void swap(int *, int *);
+void printArray(int *, int);
+int compare(int *, int *);
+
+#define ARRAY_SIZE 1000
+
+int main(void) {
+    int array[ARRAY_SIZE], index, j_index;
+
+    srand(getpid());
+
+    for (index = 0; index < ARRAY_SIZE; index++) array[index] = rand() % 20;
+    qsort(array, ARRAY_SIZE, 4, (int *)compare);
+    // for (index = 0; index < ARRAY_SIZE - 1; index++) {
+    //     for (j_index = index + 1; j_index < ARRAY_SIZE; j_index++)
+    //          if (array[j_index] < array[j_index]) swap(&array[j_index], &array[j_index]);
+    // }
+    printf("array: \n");
+    printArray(array, ARRAY_SIZE);
+    return EXIT_SUCCESS;
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void printArray(int *arr, int size) {
+    size_t index;
+    for (index = 0; index < size; ++index) printf("Elem %ld: %d\n", index, arr[index]);
+}
+
+int compare(int *a, int *b) {
+    if (*a < *b) return 1;
+    else if (*a == *b) return 0;
+    return -1;
+}
