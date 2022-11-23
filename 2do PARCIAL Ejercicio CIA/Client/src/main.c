80ec323e2afdc2d8588c819c034bf3deb18425ef
Danmuse <danifabriziodmodica@gmail.com>
2022-11-23 00:53:52 -0300

    Partial Exercise

---
+++ 2do PARCIAL Ejercicio CIA/Client/src/main.c
@@ -0,0 +1,10 @@
+// Author: Daniel Fabrizio Di Módica
+
+// "<stdio.h>" "<stdlib.h>" "<stdbool.h>" "<string.h>" "<math.h>"
+// "<fcntl.h>" "<pthread.h>" "<unistd.h>" "<sys/types.h>" "<sys/stat.h>"
+// are included below...
+#include "utils.h"
+
+int main(int argc, char *argv[]) {
+	return EXIT_SUCCESS;
+}
