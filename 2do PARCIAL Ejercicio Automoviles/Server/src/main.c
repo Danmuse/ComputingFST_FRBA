a3b8b1be2d55c2576fe98b8b15650860e0fe4da2
Danmuse <danifabriziodmodica@gmail.com>
2022-11-23 00:53:42 -0300

    Partial Exercise

---
+++ 2do PARCIAL Ejercicio Automoviles/Server/src/main.c
@@ -0,0 +1,11 @@
+// Author: Daniel Fabrizio Di Módica
+// Title: Ejercicio Automoviles
+
+// "<stdio.h>" "<stdlib.h>" "<stdbool.h>" "<string.h>" "<math.h>"
+// "<fcntl.h>" "<pthread.h>" "<unistd.h>" "<sys/types.h>" "<sys/stat.h>"
+// are included below...
+#include "utils.h"
+
+int main(int argc, char *argv[]) {
+	return EXIT_SUCCESS;
+}
