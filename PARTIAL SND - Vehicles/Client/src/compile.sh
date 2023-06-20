1462231fe4802634d39e7251e182251def7f8142
Danmuse <danifabriziodmodica@gmail.com>
2023-06-19 21:59:11 -0300

    Second partial 25/11/2022

--- PARTIAL SND - Vehicles/Client/src/compile.sh
+++
@@ -1,3 +0,0 @@
-make clean
-make
-time ../bin/result.out 127.0.0.1 open 2
--- PARTIAL SND - Vehicles/Server/src/compile.sh
+++
@@ -1,3 +0,0 @@
-make clean
-make
-time ../bin/result.out /home/ubuntu/Documents/2do_PARCIAL_Ejercicio_Automoviles/Server/assets/AutoPartes.csv
